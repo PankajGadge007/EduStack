10d2584dc7206589776911f180aab61bd10663ef
pankajpadmakar <pankajgadge007@gmail.com>
2026-01-24 00:46:24 +0530

    Initial commit - Android project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Edu Stack"
+include(":app")
+include(":quiz")
+include(":security")
+include(":designsystem")
+include(":auth")
+include(":core")
